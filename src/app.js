fc21ddbe981f48ef27c326e7670d875e53510db6
Klaudia Stopczynska <k.stopczynska@gmail.com>
2022-11-30 11:14:40 +0100

    readme update plus async function

--- src/app.js
+++ src/app.js
@@ -3,14 +3,19 @@ const quote = document.querySelector(".quote");
 const cube = document.querySelector(".cube");
 const container = document.querySelector(".container");
 
-const getAdvice = () => {
-  fetch("https://api.adviceslip.com/advice")
-    .then((res) => res.json())
-    .then((data) => {
-      quote.innerText = data.slip.advice;
-      index.innerText = data.slip.id;
-    })
-    .catch((error) => console.log("error"));
+const getAdvice = async () => {
+  try {
+  const response = await fetch("https://api.adviceslip.com/advice");
+  if (!response.ok) {
+    throw new Error('something went wrong...')
+  }
+  const data = await response.json();
+  }
+  catch (error) {
+    console.log(error)
+  }
+  quote.innerText = data.slip.advice;
+  index.innerText = data.slip.id;
 };
 getAdvice();
 
